49a011b79b8172012434a07ed60c8f1eea57d729
Shashuvo <shahariat2810@gmail.com>
2026-03-05 23:32:24 +0600

    project deployed

--- src/components/TicketSection/Tickets.jsx
+++ src/components/TicketSection/Tickets.jsx
@@ -4,11 +4,14 @@ const Tickets = ({ tickets, handleProgress, progressTask }) => {
     return (
         <div>
             <h1 className='text-[#34485A] text-2xl font-semibold mb-4'>Customer Tickets</h1>
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-6 items-stretch'>
-                {
-                    tickets.map(ticket => <TicketCard key={ticket.id} ticket={ticket} handleProgress={handleProgress} progressTask={progressTask}></TicketCard>)
-                }
-            </div>
+            {
+                tickets.length === 0 ? <p className='text-[#627382]'>No tickets left to resolve.</p> :
+                    <div className='grid grid-cols-1 md:grid-cols-2 gap-6 items-stretch'>
+                        {
+                            tickets.map(ticket => <TicketCard key={ticket.id} ticket={ticket} handleProgress={handleProgress} progressTask={progressTask}></TicketCard>)
+                        }
+                    </div>
+            }
         </div>
     );
 };
